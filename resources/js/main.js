62d7b2f4f0c544830f6f8e79774c86349edcc97e
sjkjh2 <46066384+sjkjh2@users.noreply.github.com>
2023-06-12 18:08:03 +0900

    첫번째
    업로드

---
+++ resources/js/main.js
@@ -0,0 +1,35 @@
+const valOfDIV = document.querySelector('.coming__address');
+valOfDIV.onclick = () => {
+    console.log(valOfDIV.textContent)
+    window.navigator.clipboard.writeText(valOfDIV.textContent).then(() => {
+        alert("주소가 복사 됐습니다");
+    });
+}
+
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(37.518447791216914, 127.0183165439839), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 지도를 클릭한 위치에 표출할 마커입니다
+var markerPosition = new kakao.maps.LatLng(37.518447791216914, 127.0183165439839);
+var marker = new kakao.maps.Marker({ 
+    // 지도 중심좌표에 마커를 생성합니다 
+    position: markerPosition 
+}); 
+// 지도에 마커를 표시합니다
+marker.setMap(map);
+
+// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+var mapTypeControl = new kakao.maps.MapTypeControl();
+
+// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+var zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
